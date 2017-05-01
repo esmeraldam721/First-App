e4aa65f86052f3f0fa155b58e78238c86d35d49a
Esmeralda Martinez <esmeraldam721@gmail.com>
2017-04-30 20:18:59 -0400

    S3L24

--- First Swift App/ViewController.swift
+++ First Swift App/ViewController.swift
@@ -12,18 +12,19 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var coolLabel: UILabel!
     
-    var tapCount = 0
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
     
     @IBAction func buttonTapped(_ sender: Any) {
         
-        tapCount = tapCount + 1
-        
-        if tapCount >= 30 {
-            coolLabel.text = "You tapped the button 30 times!"
-        }
+        print(text1.text!)
+        print(text2.text!)
+    
 
     }
-    
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
